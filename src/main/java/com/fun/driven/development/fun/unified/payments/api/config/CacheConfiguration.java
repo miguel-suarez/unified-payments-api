eb98922b809ce1fef768544c2bad846bb7a10e41
Miguel Suarez <mickhell@gmail.com>
2020-10-14 21:42:39 +0200

    #3 Add generated cache configuration for new entities

--- src/main/java/com/fun/driven/development/fun/unified/payments/api/config/CacheConfiguration.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/api/config/CacheConfiguration.java
@@ -1,22 +1,23 @@
 package com.fun.driven.development.fun.unified.payments.api.config;
 
-import java.time.Duration;
-
-import org.ehcache.config.builders.*;
-import org.ehcache.jsr107.Eh107Configuration;
-
-import org.hibernate.cache.jcache.ConfigSettings;
 import io.github.jhipster.config.JHipsterProperties;
-
+import io.github.jhipster.config.cache.PrefixedKeyGenerator;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.hibernate.cache.jcache.ConfigSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.info.GitProperties;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
-import io.github.jhipster.config.cache.PrefixedKeyGenerator;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -48,6 +49,22 @@ public class CacheConfiguration {
             createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.User.class.getName());
             createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Authority.class.getName());
             createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName() + ".users");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName() + ".paymentMethods");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName() + ".paymentMethodCredentials");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName() + ".paymentMethodTokens");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Merchant.class.getName() + ".unifiedPaymentTokens");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethod.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethod.class.getName() + ".paymentMethodUnavailables");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethod.class.getName() + ".paymentMethodCredentials");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethodCredential.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethodUnavailable.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethodToken.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.UnifiedPaymentToken.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Transaction.class.getName());
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.PaymentMethod.class.getName() + ".merchants");
+            createCache(cm, com.fun.driven.development.fun.unified.payments.api.domain.Currency.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
