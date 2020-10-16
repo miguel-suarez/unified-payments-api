3c256bba8d7ef8bd8b9458cfd470e8a3e27f4ac7
Miguel Suarez <mickhell@gmail.com>
2020-10-16 15:25:40 +0200

    #3 Delete BDD tests based on user resource that not part of the project anymore

--- src/test/java/com/fun/driven/development/fun/unified/payments/api/cucumber/stepdefs/StepDefs.java
+++
@@ -1,9 +0,0 @@
-package com.fun.driven.development.fun.unified.payments.api.cucumber.stepdefs;
-
-import org.springframework.test.web.servlet.ResultActions;
-
-public abstract class StepDefs {
-
-    protected ResultActions actions;
-
-}
