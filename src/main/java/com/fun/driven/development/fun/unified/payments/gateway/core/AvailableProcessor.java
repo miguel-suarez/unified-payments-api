c620308aef657c4e8454090d9a7e351bc29e5a10
Miguel Suarez <mickhell@gmail.com>
2020-10-30 15:31:43 +0100

    #5 Add payment method id to the AvailableProcessor so that we can easily fetch the merchant credentials later

--- src/main/java/com/fun/driven/development/fun/unified/payments/gateway/core/AvailableProcessor.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/gateway/core/AvailableProcessor.java
@@ -3,16 +3,22 @@ package com.fun.driven.development.fun.unified.payments.gateway.core;
 import javax.annotation.Nonnull;
 
 public enum AvailableProcessor {
-    BRAINTREE("braintree");
+    BRAINTREE("braintree", 1L);
 
     private String reference;
+    private Long paymentMethodId;
 
-    AvailableProcessor(String reference) {
+    AvailableProcessor(String reference, Long paymentMethodId) {
         this.reference = reference;
+        this.paymentMethodId = paymentMethodId;
     }
 
     public String getReference() {
         return reference;
+    }
+
+    public Long getPaymentMethodId() {
+        return paymentMethodId;
     }
 
     public static AvailableProcessor fromReference(@Nonnull String reference) {
