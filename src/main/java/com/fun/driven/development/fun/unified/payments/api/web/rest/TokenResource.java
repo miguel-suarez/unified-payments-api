e6646516e4fdbcf2a4488a69162cc57ff7045455
Miguel Suarez <mickhell@gmail.com>
2020-10-31 17:33:08 +0100

    #4 Update API to return 200 instead of 201 as it's not actually creating a resource that can be latter accessed in a REST manner. Add validations. Store generated token in DB

--- src/main/java/com/fun/driven/development/fun/unified/payments/api/web/rest/TokenResource.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/api/web/rest/TokenResource.java
@@ -1,15 +1,20 @@
 package com.fun.driven.development.fun.unified.payments.api.web.rest;
 
+import com.fun.driven.development.fun.unified.payments.api.service.MerchantService;
+import com.fun.driven.development.fun.unified.payments.api.service.TokenGenerator;
+import com.fun.driven.development.fun.unified.payments.api.service.UnifiedPaymentTokenService;
+import com.fun.driven.development.fun.unified.payments.api.service.dto.MerchantDTO;
 import com.fun.driven.development.fun.unified.payments.api.service.dto.UnifiedPaymentTokenDTO;
 import com.fun.driven.development.fun.unified.payments.api.web.rest.vm.CardVM;
-import io.github.jhipster.web.util.HeaderUtil;
+import com.fun.driven.development.fun.unified.payments.api.web.rest.vm.TokenVM;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,18 +23,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.Optional;
 
 @RestController
 @Api(value = "token")
 @RequestMapping("/api")
 public class TokenResource {
 
-    private static final String ENTITY_NAME = "token";
+    @Autowired
+    private MerchantService merchantService;
 
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
+    @Autowired
+    private UnifiedPaymentTokenService tokenService;
+
+    @Autowired
+    private TokenGenerator tokenGenerator;
 
     /**
      * POST /v1/unified/tokens : Generate a unified payment token for a card
@@ -42,27 +50,76 @@ public class TokenResource {
      *         or User isn&#39;t allow to perform the requested action (status code 403)
      */
     @ApiOperation(value = "Generate a unified payment token for a card", nickname = "createToken",
-                  notes = "", response = UnifiedPaymentTokenDTO.class,
+                  response = TokenVM.class,
                   authorizations = {@Authorization(value = "basic_auth")},
                   tags={ "token" })
     @ApiResponses(value = {
-        @ApiResponse(code = 201, message = "Successfully created", response = UnifiedPaymentTokenDTO.class),
-        @ApiResponse(code = 400, message = "Invalid card supplied"),
-        @ApiResponse(code = 401, message = "Authentication information is missing or invalid"),
-        @ApiResponse(code = 403, message = "User isn't allow to perform the requested action"),
-        @ApiResponse(code = 500, message = "Sorry we can't process this request at the moment")})
+        @ApiResponse(code = 200, message = "Successfully created", response = TokenVM.class),
+        @ApiResponse(code = 400, message = "Invalid card supplied", response = TokenVM.class),
+        @ApiResponse(code = 401, message = "Authentication information is missing or invalid", response = TokenVM.class),
+        @ApiResponse(code = 403, message = "User isn't allow to perform the requested action", response = TokenVM.class),
+        @ApiResponse(code = 500, message = "Sorry we can't process this request at the moment", response = TokenVM.class)})
     @PostMapping(value = "/v1/unified/tokens",
         produces = { "application/json" },
         consumes = { "application/json" })
-    public ResponseEntity<UnifiedPaymentTokenDTO> createToken(@ApiParam(value = "Merchant reference", required=true)
-                                                                  @RequestHeader(value="X-Unified-Payments-Merchant-Reference") String merchantReference,
-                                                              @ApiParam(value = "Card for which the token will be generated", required=true, name = "Card")
-                                                                  @Valid @RequestBody CardVM card)
-                                                              throws URISyntaxException {
-        UnifiedPaymentTokenDTO result = new UnifiedPaymentTokenDTO();
-        result.setId(1L);
-        return ResponseEntity.created(new URI("/api/v1/unified/tokens/" + result.getId()))
-                             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-                             .body(result);
+    public ResponseEntity<TokenVM> createToken(@ApiParam(value = "Merchant reference", required=true)
+                                                   @RequestHeader(value="X-Unified-Payments-Merchant-Reference") String merchantReference,
+                                               @ApiParam(value = "Card for which the token will be generated", required=true, name = "Card")
+                                                   @Valid @RequestBody CardVM card) {
+        Optional<ResponseEntity<TokenVM>> validationError = validateInput(merchantReference, card);
+        if (validationError.isPresent()) return validationError.get();
+
+        UnifiedPaymentTokenDTO tokenDTO = tokenGenerator.of(card);
+
+        tokenService.save(fillIds(merchantReference,tokenDTO));
+        return ResponseEntity.ok().body(TokenVM.from(tokenDTO));
     }
+
+    private UnifiedPaymentTokenDTO fillIds(String merchantReference, UnifiedPaymentTokenDTO tokenDTO) {
+        Optional<MerchantDTO> merchantDTO = merchantService.findOneByReference(merchantReference);
+        Long merchantId = merchantDTO.isPresent() ? merchantDTO.get().getId() : -1;
+        tokenDTO.setMerchantId(merchantId);
+        return tokenDTO;
+    }
+
+    private Optional<ResponseEntity<TokenVM>> validateInput(String merchantReference, CardVM card) {
+        Optional<ResponseEntity<TokenVM>> unauthorized = validateMerchant(merchantReference);
+        if (unauthorized.isPresent()) return unauthorized;
+
+        Optional<ResponseEntity<TokenVM>> badRequest = validateUserMerchant(merchantReference);
+        if (badRequest.isPresent()) return badRequest;
+
+        return validateCard(card);
+    }
+
+    private Optional<ResponseEntity<TokenVM>> validateMerchant(String merchantReference) {
+        Optional<MerchantDTO> merchantDTO = merchantService.findOneByReference(merchantReference);
+        if (merchantDTO.isEmpty()) {
+            TokenVM result = new TokenVM().errorMessage("Authentication information is missing or invalid");
+            return Optional.of(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result));
+        }
+        return Optional.empty();
+    }
+
+    private Optional<ResponseEntity<TokenVM>> validateUserMerchant(String merchantReference) {
+        if (merchantService.isNotAuthorizedUserOwnedByMerchant(merchantReference)) {
+            TokenVM result = new TokenVM().errorMessage("User doesn't have enough credentials to access resource");
+            return Optional.of(ResponseEntity.status(HttpStatus.FORBIDDEN).body(result));
+        }
+        return Optional.empty();
+    }
+
+    private Optional<ResponseEntity<TokenVM>> validateCard(CardVM card) {
+        if (isInvalidCardNumber(card)) {
+            TokenVM result = new TokenVM().errorMessage("User doesn't have enough credentials to access resource");
+            return Optional.of(ResponseEntity.badRequest().body(result));
+        }
+        return Optional.empty();
+    }
+
+    private boolean isInvalidCardNumber(CardVM card) {
+        //TODO implement
+        return false;
+    }
+
 }
