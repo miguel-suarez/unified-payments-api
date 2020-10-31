74f31b55d41b21035939ab2f918aa064c83a58c0
Miguel Suarez <mickhell@gmail.com>
2020-10-31 15:10:52 +0100

    #5 Add validation of created transaction on the happy flow. Use a valid merchant-user in the test cases

--- src/test/java/com/fun/driven/development/fun/unified/payments/api/web/rest/PaymentResourceIT.java
+++ src/test/java/com/fun/driven/development/fun/unified/payments/api/web/rest/PaymentResourceIT.java
@@ -1,6 +1,8 @@
 package com.fun.driven.development.fun.unified.payments.api.web.rest;
 
 import com.fun.driven.development.fun.unified.payments.api.FunUnifiedPaymentsApiApp;
+import com.fun.driven.development.fun.unified.payments.api.service.TransactionService;
+import com.fun.driven.development.fun.unified.payments.api.service.dto.TransactionDTO;
 import com.fun.driven.development.fun.unified.payments.api.web.rest.vm.SaleVM;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +17,9 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,25 +37,40 @@ public class PaymentResourceIT {
     @Autowired
     private MockMvc restMerchantMockMvc;
 
+    @Autowired
+    private TransactionService transactionService;
+
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void sale() throws Exception {
+        int tableSizeBefore = transactionService.findAll().size();
+
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR")
-                                    .token("Meadow");
+                                    .token("Racket");
         restMerchantMockMvc.perform(post((ENDPOINT_URL))
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .content(TestUtil.convertObjectToJsonBytes(saleVM))
                                     .header(Constants.MERCHANT_HEADER,"hardware"))
                            .andExpect(status().isOk());
+
+        List<TransactionDTO> transactions = transactionService.findAll();
+        assertThat(transactions).hasSize(tableSizeBefore + 1);
+        TransactionDTO transaction = transactions.get(transactions.size() -1);
+        assertThat(transaction.getAmount()).isEqualTo(100L);
+        assertThat(transaction.getProcessorResult()).isNotEmpty();
+        assertThat(transaction.getFunReference()).isNotEmpty();
+        //TODO validate the rest of the fields to make sure the tx is created properly
     }
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     // If no currencyCode is given, EUR is used as a default value
     void saleWithoutCurrencyCode() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(101L)
-                                    .token("Meadow");
+                                    .token("Racket");
         restMerchantMockMvc.perform(post((ENDPOINT_URL))
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .content(TestUtil.convertObjectToJsonBytes(saleVM))
@@ -74,6 +94,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithWrongMerchantReference() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR")
@@ -87,6 +108,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithWrongPaymentProcessor() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR")
@@ -101,6 +123,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithWrongMerchantCredentials() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR")
@@ -128,6 +151,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithoutAmount() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(null)
                                     .currencyIsoCode("EUR")
@@ -141,6 +165,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithTokenOfDifferentMerchant() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(102L)
                                     .currencyIsoCode("EUR")
@@ -154,6 +179,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithoutToken() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR");
@@ -166,6 +192,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithInvalidToken() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("EUR")
@@ -179,6 +206,7 @@ public class PaymentResourceIT {
 
     @Test
     @Transactional
+    @WithUserDetails(value = "system", userDetailsServiceBeanName = "userDetailsService")
     void saleWithInvalidCurrency() throws Exception {
         SaleVM saleVM = new SaleVM().amountInCents(100L)
                                     .currencyIsoCode("OOO")
