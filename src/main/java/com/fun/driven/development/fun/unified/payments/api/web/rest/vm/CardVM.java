a6be7d062ad481d3213032ae8049db3f0fac9abe
Miguel Suarez <mickhell@gmail.com>
2020-10-31 20:48:35 +0100

    #4 Move card validations to CardVM

--- src/main/java/com/fun/driven/development/fun/unified/payments/api/web/rest/vm/CardVM.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/api/web/rest/vm/CardVM.java
@@ -1,18 +1,25 @@
 package com.fun.driven.development.fun.unified.payments.api.web.rest.vm;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.apache.commons.validator.routines.checkdigit.LuhnCheckDigit;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 @ApiModel(description = "Card for which the token will be generated")
 @JsonRootName(value = "Card")
 public class CardVM {
+
+    @JsonIgnore
+    private static LuhnCheckDigit luhnCheck = new LuhnCheckDigit();
 
     @JsonProperty("cardNumber")
     @ApiModelProperty(example = "370000000000002", required = true)
@@ -31,6 +38,7 @@ public class CardVM {
     @NotNull
     @Min(2020) @Max(2100)
     private Integer expiryYear;
+
 
     public String getCardNumber() {
         return cardNumber;
@@ -57,5 +65,17 @@ public class CardVM {
     public CardVM expiryYear(Integer expiryYear) {
         this.expiryYear = expiryYear;
         return this;
+    }
+
+    @JsonIgnore
+    public boolean hasInvalidCardNumber() {
+        return ! luhnCheck.isValid(cardNumber);
+    }
+
+    @JsonIgnore
+    public boolean isExpired() {
+        LocalDate firstDayMonthExpiry = LocalDate.of(expiryYear, expiryMonth, 1);
+        LocalDate expiryDate = firstDayMonthExpiry.with(TemporalAdjusters.lastDayOfMonth());
+        return expiryDate.isBefore(LocalDate.now());
     }
 }
