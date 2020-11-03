265273b74afc37ad88e9b502a8a5756889f77897
Miguel Suarez <mickhell@gmail.com>
2020-11-03 09:59:34 +0100

    #4 Adjusted mock implementation of the Vault

--- src/main/java/com/fun/driven/development/fun/unified/payments/vault/PaymentDetailsVaultMock.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/vault/PaymentDetailsVaultMock.java
@@ -1,19 +1,35 @@
 package com.fun.driven.development.fun.unified.payments.vault;
 
+import com.fun.driven.development.fun.unified.payments.api.domain.enumeration.PaymentMethodType;
+import com.fun.driven.development.fun.unified.payments.api.service.UnifiedPaymentTokenService;
+import com.fun.driven.development.fun.unified.payments.api.service.dto.UnifiedPaymentTokenDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentDetailsVaultMock implements PaymentDetailsVault {
 
-    public UnifiedToken tokenize(Card card) {
-        //TODO implement
-        return new UnifiedToken("abcdef123456");
+    @Autowired
+    private TokenGenerator tokenGenerator;
+
+    @Autowired
+    private UnifiedPaymentTokenService tokenService;
+
+    @Override
+    public UnifiedPaymentTokenDTO tokenize(Long merchantId, Card card) {
+        String token = "abcdef123456";
+        String payload = "";
+        UnifiedPaymentTokenDTO tokenDTO = new UnifiedPaymentTokenDTO(token).merchantId(merchantId)
+                                                                           .payload(payload)
+                                                                           .type(PaymentMethodType.CARD);
+        tokenService.save(tokenDTO);
+        return tokenDTO;
     }
 
-    public Card retrieveCard(UnifiedToken token) {
-        //TODO implement
+    public Card retrieveCard(String token) {
         return new Card().setNumber("4111111111111111")
                          .setExpirationMonth(5)
                          .setExpirationYear(2030);
     }
+
 }
