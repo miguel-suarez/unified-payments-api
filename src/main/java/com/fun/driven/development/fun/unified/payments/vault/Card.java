11e260c058eb5c714a461ea4aad9182caa73eb16
Miguel Suarez <mickhell@gmail.com>
2020-11-03 17:15:28 +0100

    #8 Add methods to marshall and unmarshall json <-> object

--- src/main/java/com/fun/driven/development/fun/unified/payments/vault/Card.java
+++ src/main/java/com/fun/driven/development/fun/unified/payments/vault/Card.java
@@ -1,8 +1,21 @@
 package com.fun.driven.development.fun.unified.payments.vault;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAdjusters;
 
 public class Card {
+
+    private static final Logger log = LoggerFactory.getLogger(Card.class);
 
     @NotNull
     private String number;
@@ -10,6 +23,8 @@ public class Card {
     private Integer expirationMonth;
     @NotNull
     private Integer expirationYear;
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     public String getNumber() {
         return number;
@@ -33,6 +48,7 @@ public class Card {
         return expirationYear;
     }
 
+    @JsonIgnore
     public String getExpirationMMSlashYYYY() {
         return String.format("%02d", expirationMonth) + "/" + String.format("%04d", expirationYear);
     }
@@ -42,10 +58,33 @@ public class Card {
         return this;
     }
 
+    @JsonIgnore
+    public Instant lastUsableUTCInstant() {
+        LocalDate firstDayMonthExpiry = LocalDate.of(expirationYear, expirationMonth, 1);
+        LocalDateTime expiryDateTime = firstDayMonthExpiry.with(TemporalAdjusters.lastDayOfMonth())
+                                                          .atTime(23,59,59);
+        return expiryDateTime.toInstant(ZoneOffset.UTC);
+    }
+
     @Override
     public String toString() {
-        return "Card{" + "number='xxxxxxxxxxxxx\'" +
+        return "Card{" + "number='xxxxxxxxxxxxx'" +
             ", expirationMonth=" + expirationMonth +
             ", expirationYear=" + expirationYear + '}';
+    }
+
+    @JsonIgnore
+    protected String toJson() {
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            log.error("Can't parse object to json", e);
+            return "";
+        }
+    }
+
+    @JsonIgnore
+    protected static Card fromJson(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, Card.class);
     }
 }
